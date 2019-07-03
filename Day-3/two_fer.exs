385ad8cfa375e2091bcf05ed9c0207a325b3fb58
dibyanshu <dibyanshu@gamezop.co>
2019-07-03 11:43:48 +0530

    removed error handling

--- Day-3/two_fer.exs
+++ Day-3/two_fer.exs
@@ -5,5 +5,4 @@ defmodule TwoFer do
   @spec two_fer(String.t()) :: String.t()
   def two_fer(), do: "One for you, one for me"
   def two_fer(name) when is_binary(name), do: "One for #{name}, one for me"
-  def two_fer(_), do: raise(FunctionClauseError)
 end
