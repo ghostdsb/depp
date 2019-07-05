8cefc202bdcdc42ce6d20c9a9378fe62696574a8
dibyanshu <dibyanshu@gamezop.co>
2019-07-05 11:49:11 +0530

    changed regex fn to decrease code

--- Day-5/acronym.exs
+++ Day-5/acronym.exs
@@ -5,22 +5,9 @@ defmodule Acronym do
   """
   @spec abbreviate(String.t()) :: String.t()
   def abbreviate(string) do
-    string
-    |> String.split( ~r{[^A-Za-z]}, trim: true)
-    |> Enum.reduce("", fn x,ac -> ac<>abbreviate_word(x) end)
-  end
-
-  defp is_upcase?(char), do: String.upcase(char) == char
-
-  defp abbreviate_word(word) do
-    word
-    |> String.codepoints
-    |> Enum.reduce("",fn x, ac -> cond do
-      ac == "" -> ac<>String.upcase(x)
-      is_upcase?(x)-> ac<>x
-      true -> ac
-      end
-    end)
+    Regex.scan(~r/\b\w|[A-Z]/, string)
+    |> List.to_string
+    |> String.upcase
   end
 
 end
