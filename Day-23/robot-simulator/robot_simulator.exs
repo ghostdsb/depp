91468ab34cfa679a0ed7839434d9d663ad4a302f
dibyanshu <dibyanshu@gamezop.co>
2019-07-26 21:45:46 +0530

    w/o OTP

--- Day-23/robot-simulator/robot_simulator.exs
+++ Day-23/robot-simulator/robot_simulator.exs
@@ -1,12 +1,21 @@
 defmodule RobotSimulator do
+
+  @directions [:north, :east, :west, :south]
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
+  def create(), do: %{:direction => :north,   :position => {0,0}}
+  def create(direction, position = {_,_}) do
+    cond do
+      not valid_direction?(direction) -> {:error, "invalid direction"}
+      not valid_position?(position)   -> {:error, "invalid position"}
+      true ->  %{:direction => direction,   :position => position}   
+    end
   end
+  def create(_, _), do: {:error, "invalid position"}
 
   @doc """
   Simulate the robot's movement given a string of instructions.
@@ -15,6 +24,10 @@ defmodule RobotSimulator do
   """
   @spec simulate(robot :: any, instructions :: String.t()) :: any
   def simulate(robot, instructions) do
+    case valid_instructions?(instructions) do
+      true -> move(robot, instructions)
+      false -> {:error, "invalid instruction"}
+    end
   end
 
   @doc """
@@ -24,6 +37,7 @@ defmodule RobotSimulator do
   """
   @spec direction(robot :: any) :: atom
   def direction(robot) do
+    robot[:direction]
   end
 
   @doc """
@@ -31,5 +45,59 @@ defmodule RobotSimulator do
   """
   @spec position(robot :: any) :: {integer, integer}
   def position(robot) do
+    robot[:position]
+  end
+  
+  defp valid_direction?(direction) do
+    @directions
+    |> Enum.any?(&(&1===direction))
+  end
+
+  defp valid_position?({x,y}) do
+    is_integer(x) && is_integer(y)
+  end
+  
+  defp valid_instructions?(instructions) do
+    instructions
+    |> String.codepoints
+    |> Enum.any?(&(&1 !== "R" and &1 !== "L" and &1 !== "A"))
+    |> (&(!&1)).()
+  end
+
+  defp move(robot, instructions) do
+    instructions
+    |> String.codepoints
+    |> Enum.reduce(robot,fn x,state->update(state,x) end)
+  end
+
+  defp update(robot, "R") do
+    Map.update!(robot, :direction, fn dir -> 
+      case dir do
+        :north -> :east
+        :east -> :south
+        :south -> :west
+        :west -> :north
+      end
+    end)
+  end
+
+  defp update(robot, "L") do
+  Map.update!(robot, :direction, fn dir -> 
+      case dir do
+        :north -> :west
+        :east -> :north
+        :south -> :east
+        :west -> :south
+      end
+    end)
+  end
+
+  defp update(robot, "A") do
+    case direction(robot) do
+      :north -> Map.update!(robot, :position, fn {x,y}-> {x,y+1} end)
+      :east -> Map.update!(robot, :position, fn {x,y}-> {x+1,y} end)
+      :south -> Map.update!(robot, :position, fn {x,y}-> {x,y-1} end)
+      :west -> Map.update!(robot, :position, fn {x,y}-> {x-1,y} end)
+    end
   end
 end
